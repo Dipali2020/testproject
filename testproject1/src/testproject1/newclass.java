62e95746796bd8e58c8f137cfb2a08aa35ebd1d4
Kiran <Kiran@Lenovo-PC>
2020-02-18 02:59:07 +1200

    second commit

--- testproject1/src/testproject1/newclass.java
+++ testproject1/src/testproject1/newclass.java
@@ -4,7 +4,8 @@ public class newclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("hello");
+System.out.println("my frist seleniium code");
 	}
 
 }
